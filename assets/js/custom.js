9de76314643a03b44ee4c3317e86bf8bce60605f
devavology <145644105+devavology@users.noreply.github.com>
2025-01-28 19:10:08 +0530

    add

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,12 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
-    $(function() {
-        var header = $(".navbar");
-      
-        $(window).scroll(function() {    
-            var scroll = $(window).scrollTop();
-            if (scroll >= 50) {
-                header.addClass("fixed");
-            } else {
-                header.removeClass("fixed");
-            }
-        });
-      
+$(document).ready(function () {
+    var header = $(".site-header");
+
+    $(window).on("scroll", function () {
+        var scroll = $(this).scrollTop();
+        if (scroll >= 50) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
     });
 });
